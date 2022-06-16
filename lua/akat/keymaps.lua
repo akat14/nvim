d4ba0558f0657b8a4b71332b591bd0034b05b26f
Valentin Borisov <akata14@gmail.com>
2022-06-16 18:20:52 +0300

    git keybinds

--- lua/akat/keymaps.lua
+++ lua/akat/keymaps.lua
@@ -25,8 +25,6 @@ keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 
--- Insert --
-
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
@@ -37,3 +35,8 @@ keymap("v", ">", ">gv", opts)
 -- Telescope --
 keymap("n", "<c-t>", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>f", "<cmd>Telescope live_grep<cr>", opts)
+
+--Git
+keymap("n", "hp", "<cmd>Gitsigns prev_hunk<cr>", opts)
+keymap("n", "hn", "<cmd>Gitsigns next_hunk<cr>", opts)
+keymap("n", "gh", "<cmd>Gitsigns preview_hunk<cr>", opts)
